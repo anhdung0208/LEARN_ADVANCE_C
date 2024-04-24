774d0a2dc8531ca725352240660a1ad5d3d97b63
AnhDung <haha>
2024-04-24 14:45:10 +0700

    Initial commit

--- test/LESSON 1/Lesson_1_Macro.c
+++ test/LESSON 1/Lesson_1_Macro.c
@@ -1,58 +1,77 @@
 #include<stdio.h>
-#include "fucntion.h"
+//#include<stdint.h>
+//#include "fucntion.h"
 
 #define MAX 20
 #define sum(a,b) a+b
 
-// Để tránh việc xảy ra lỗi khi định nghĩa 2 lần macro sẽ khai báo 2 hàm có cùng tên nên xảy ra lỗi nên cần làm cách 2 (Test bằng gcc -E Lesson_1_Macro.c -o Lesson_1_Macro.i) 
-
-// Cách 1
-#define DISPLAY(cmd) \
-void print_messege() \
-{                     \
- printf("cmd");\
-}
-
-// Cách 2
-
-#define DISPLAY(nameFunc,cmd) \
-void nameFunc() \
-{                     \
- printf("cmd");\
-}
-
-// Cách 3
-#define DISPLAY(nameFunc,cmd) \
-void nameFunc() \
-{                     \
- printf("#cmd");\ // với dấu thăng khi ta khởi tạo macro thì ta k cần dấu ngoặc kép để thể hiện là 1 chuỗi bởi vì # là format của chuỗi 
-}
-
-// Cách 4
-//Tạo biến với macro chú ý khi viết macro trên 2 dòng thêm dấu "\""
-#define CREATE_VARIABLE(name)\
-int int##_name;\
-double double##_name;
-
 /**
- * Cách 5
+ * Macro chỉ được định nghĩa 1 lần
  * 
  */
 
 
 
+// Để tránh việc xảy ra lỗi khi định nghĩa 2 lần macro sẽ khai báo 2 hàm có cùng tên nên xảy ra lỗi nên cần làm cách 2 (Test bằng gcc -E Lesson_1_Macro.c -o Lesson_1_Macro.i) 
 
-DISPLAY(test1,"This is the ");
-DISPLAY(test2,"This is the 1 ");
+// Cách 1
+// #define DISPLAY(cmd) \
+// void print_messege() \
+// {                     \
+//  printf("cmd");\
+// }
+
+// Cách 2
+
+// #define DISPLAY(nameFunc,cmd) \
+// void nameFunc() \
+// {                     \
+//  printf("cmd");\
+// }
+
+// Cách 3
+// #define DISPLAY(nameFunc,cmd) \
+// void nameFunc() \
+// {                     \
+//  printf("#cmd");\ // với dấu thăng khi ta khởi tạo macro thì ta k cần dấu ngoặc kép để thể hiện là 1 chuỗi bởi vì # là format của chuỗi 
+// }
+
+// Cách 4
+//Tạo biến với macro chú ý khi viết macro trên 2 dòng thêm dấu "\""
+// #define CREATE_VARIABLE(name)\
+// int int##_name;\
+// double double##_name;
+
+/**
+ * Cách 5: IF ELSE với Macro
+ * 
+ * 
+ */
+
+#define STM32F103 0
+#define STM32F104 1 
+
+
+/**
+ * Var_list với macro dùng để khai báo và sử dụng tạo ra một hàm với các tham số truyền vào linh hoạt
+ * 
+ */
+
+#define SUM(...) _VA_ARGS_\
+int arr[] = {_VA_ARGS_} \
+
+
+//DISPLAY(test1,"This is the ");
+//DISPLAY(test2,"This is the 1 ");
 
 int main()
 {
-     test1();
-     test2();
+     //test1();
+     //test2();
 
      //int ARR[MAX];
      //int result = sum(1,2);
       
-     //printf( "%d",result); 
+     printf( "%d",3); 
      //print_test();
 }
