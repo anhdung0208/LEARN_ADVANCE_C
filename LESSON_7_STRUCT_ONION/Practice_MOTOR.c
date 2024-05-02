83c6206492911f9521b077ced2cd5ad07a3bfa34
AnhDung <haha>
2024-05-03 00:28:02 +0700

    Initial commit

--- LESSON_7_STRUCT_ONION/Practice_MOTOR.c
+++ LESSON_7_STRUCT_ONION/Practice_MOTOR.c
@@ -8,9 +8,12 @@ typedef struct {
     void (*changSpeed)(int, int SPEED);
 }controlMotor;
 
-#define INIT_MOTOR(nameMotor, PIN_NUMBER) \
-    PIN PIN_##nameMotor = PIN_NUMBER;         \
-    controlMotor nameMotor = {&startMotor, &stopMotor, &changeMotor}; 
+// #define INIT_MOTOR(nameMotor, PIN_NUMBER) \
+//     PIN PIN_##nameMotor = PIN_NUMBER;         \
+//     controlMotor nameMotor = {&startMotor, &stopMotor, &changeMotor}; 
+
+
+  
 
 
 
@@ -29,11 +32,21 @@ void changeMotor(int GPIO, int SPEED)
     printf("\nSPEED CHANGE:%dd",SPEED);
 }
 
- 
+ void init_motor_v2(controlMotor *name_motor)
+{
+    (*name_motor).start = &startMotor;
+    (*name_motor).stop = &stopMotor;
+    (*name_motor).changSpeed = &changeMotor;
+}  
 
 int main()
 {
-INIT_MOTOR(motor_A,23);
+//INIT_MOTOR(motor_A,23);
+controlMotor motor_A;
+
+init_motor_v2(&motor_A);
+
 motor_A.start(1);
+
 
 }
